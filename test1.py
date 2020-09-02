5bb3a37bb3be4b7b44cc49e8eb43038ef9679d90
NiranjanK <kulkniranjan03@gmail.com>
2020-09-02 11:44:05 -0500

    changes

--- test1.py
+++ test1.py
@@ -1,56 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-# !pip install azure-storage-blob
-# !pip install xlrd
-
-
-# In[2]:
-
-
-# !pip install --upgrade pip --user
-
-
-# In[3]:
-
-
-# !pip uninstall azure
-
-
-# In[4]:
-
-
-# !pip uninstall azure-storage-file-share
-
-
-# In[5]:
-
-
-# !pip install azure-storage-file-share --pre --user
-
-
-# In[6]:
-
-
-# !pip freeze > requirements.txt
-
-
-# In[7]:
-
-
-# !cat requirements.txt
-
-
 # In[8]:
 
 
 import os, uuid
 from io import StringIO
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-# from azure.identity import DefaultAzureCredential
+from azure.storage.blob import generate_blob_sas, AccountSasPermissions
+from datetime import datetime, timedelta
+import pandas as pd 
 
 
 # In[44]:
@@ -61,7 +20,6 @@ account_name = 'graybardata'
 account_key = 'V+zUSJhOAVNU62mk55jRtwWNucjJ7ndkLvMGg/t0sLPEr0P4w9y3GphpXJ/2O56jIyfwHa2o7e8PtAI0MRyCCw=='
 container_name = 'csvfiles'
 blob_name = 'Schneider_one_to_many.xlsx'
-from azure.storage.blob import ContainerClient
 
 container = ContainerClient.from_connection_string(conn_str=connection_string, container_name="csvfiles")
 
@@ -73,24 +31,16 @@ for blob in blob_list:
 # In[10]:
 
 
-from azure.storage.blob import BlobClient
-
 blob = BlobClient.from_connection_string(conn_str=connection_string, container_name="csvfiles", blob_name="Schneider_one_to_many.xlsx")
 
 with open("./Schneider_one_to_many.xlsx", "wb") as my_blob:
     blob_data = blob.download_blob()
     blob_data.readinto(my_blob)
-# import pandas as pd
-# df = pd.read_excel('Schneider_one_to_many.xlsx')
-# df
 
 
 # In[50]:
 
 
-from azure.storage.blob import generate_blob_sas, AccountSasPermissions
-
-# def scan_product():
 url = f"https://{account_name}.blob.core.windows.net/{container_name}/{blob_name}"
 sas_token = generate_blob_sas(
     account_name=account_name,
@@ -123,8 +73,6 @@ print(url_with_sas)
 # In[51]:
 
 
-import pandas as pd 
-print(source)
 df = pd.read_excel(url_with_sas)
 print(df.head())
 
