513797ac889a44e00f7a84b9b377863e330e521f
NiranjanK <kulkniranjan03@gmail.com>
2020-09-02 09:36:52 -0500

    made changes

--- test1.py
+++ test1.py
@@ -1,20 +1,50 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[33]:
+# In[ ]:
 
 
-get_ipython().system('pip3 install azure-storage-blob')
+get_ipython().system('pip install azure-storage-blob')
 get_ipython().system('pip install xlrd')
 
 
-# In[8]:
+# In[ ]:
 
 
-get_ipython().system('pip install azure-storage-file')
+get_ipython().system('pip install --upgrade pip --user')
 
 
-# In[22]:
+# In[ ]:
+
+
+# !pip uninstall azure
+
+
+# In[ ]:
+
+
+# !pip uninstall azure-storage-file-share
+
+
+# In[ ]:
+
+
+# !pip install azure-storage-file-share --pre --user
+
+
+# In[ ]:
+
+
+# !pip freeze > requirements.txt
+
+
+# In[ ]:
+
+
+# !cat requirements.txt
+
+
+# In[ ]:
 
 
 import os, uuid
@@ -23,37 +53,12 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 # from azure.identity import DefaultAzureCredential
 
 
-# In[16]:
-
-
-get_ipython().system('pip freeze')
-
-
 # In[ ]:
-
-
-# from azure.storage.blob import BlobServiceClient
-
-# service = BlobServiceClient(account_url="DefaultEndpointsProtocol=https;AccountName=graybardata;AccountKey=hOOZv/LxplEi8W5FIaPgF8mSajjIDFHvYViXfS04Msje7NRUmDYB4xY76yqFOYGIBOACl+dGF+fidaFgwAmX2w==;EndpointSuffix=core.windows.net", credential=credential)
-
-
-# In[ ]:
-
-
-# block_blob_service = BlockBlobService(account_name='meetpythonstorage', account_key='duOguiKnYb6ZEbJC6BftWqA2lcH67dWkmCSEJj+KxOTOHCNPeV7r4oO6feTw7gSSoFGKHryL4yqSVWlEkm6jWg==')
-
-
-# In[ ]:
-
-
-# blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
-# container_client = blob_service_client.get_container_client("graybardata")
-
-
-# In[17]:
 
 
 connection_string  = 'DefaultEndpointsProtocol=https;AccountName=graybardata;AccountKey=V+zUSJhOAVNU62mk55jRtwWNucjJ7ndkLvMGg/t0sLPEr0P4w9y3GphpXJ/2O56jIyfwHa2o7e8PtAI0MRyCCw==;EndpointSuffix=core.windows.net'
+account_name = 'graybardata'
+access_key = 'V+zUSJhOAVNU62mk55jRtwWNucjJ7ndkLvMGg/t0sLPEr0P4w9y3GphpXJ/2O56jIyfwHa2o7e8PtAI0MRyCCw=='
 from azure.storage.blob import ContainerClient
 
 container = ContainerClient.from_connection_string(conn_str=connection_string, container_name="csvfiles")
@@ -63,16 +68,7 @@ for blob in blob_list:
     print(blob.name + '\n')
 
 
-# In[18]:
-
-
-block_blob_service = BlobServiceClient(account_url='https://graybardata.blob.core.windows.net/csvfiles')
-
-blob = block_blob_service.get_blob_as_bytes('csvfiles', 'Schneider_one_to_many.xlsx')
-print(blob.content)
-
-
-# In[34]:
+# In[ ]:
 
 
 from azure.storage.blob import BlobClient
@@ -82,27 +78,46 @@ blob = BlobClient.from_connection_string(conn_str=connection_string, container_n
 with open("./Schneider_one_to_many.xlsx", "wb") as my_blob:
     blob_data = blob.download_blob()
     blob_data.readinto(my_blob)
-import pandas as pd
-df = pd.read_excel('Schneider_one_to_many.xlsx')
-df
+# import pandas as pd
+# df = pd.read_excel('Schneider_one_to_many.xlsx')
+# df
 
 
-# In[38]:
+# In[ ]:
+
+
+import azure.storage.fileshare
+from azure.storage.fileshare import ShareServiceClient
+share_service_client = ShareServiceClient.from_connection_string(self.connection_string)
+
+# Create a SAS token to use to authenticate a new client
+from azure.storage.fileshare import generate_account_sas, ResourceTypes, AccountSasPermissions
+
+sas_token = generate_account_sas(
+    self.account_name,
+    self.access_key,
+    resource_types=ResourceTypes(service=True),
+    permission=AccountSasPermissions(read=True),
+    expiry=datetime.utcnow() + timedelta(hours=1)
+)
+
+
+# In[ ]:
 
 
 import pandas as pd 
-source = 'https://graybardata.blob.core.windows.net/csvfiles/Schneider_one_to_many.xlsx?sp=r&st=2020-08-30T17:20:23Z&se=2020-08-31T01:20:23Z&spr=https&sv=2019-12-12&sr=b&sig=st2XbS7v8rSA39eddxD7Ng61%2BUdvs%2Bp5kHZDsw%2BicTI%3D'
+source = str(sas_token)
 df = pd.read_excel(source)
 print(df.head())
 
 
-# In[40]:
+# In[ ]:
 
 
 df.to_csv('test.csv', index = False)
 
 
-# In[41]:
+# In[ ]:
 
 
 blob_service_client = BlobServiceClient.from_connection_string('DefaultEndpointsProtocol=https;AccountName=graybardata;AccountKey=V+zUSJhOAVNU62mk55jRtwWNucjJ7ndkLvMGg/t0sLPEr0P4w9y3GphpXJ/2O56jIyfwHa2o7e8PtAI0MRyCCw==;EndpointSuffix=core.windows.net')
